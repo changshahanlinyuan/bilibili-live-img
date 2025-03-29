fb0d037e224f4e79342c4d3443ebf34c1fbe065a
xiaochen004hao <xiaochen004hao@163.com>
2025-03-30 05:48:27 +0800

    yes

--- bilibiliLiveImg/settings.py
+++ bilibiliLiveImg/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+%1fe9n_-q(kte4#j2ekoxn1bqc!&ysoex+bz78^^i%ss0sko9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bilibili-live-img.up.railway.app']
 
 
 # Application definition
@@ -117,7 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
